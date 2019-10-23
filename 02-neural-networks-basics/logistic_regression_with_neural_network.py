5a623dda8e5be9d49c5dd81aae1c507e5340dd84
johnptmcdonald <johnptmcdonald@gmail.com>
2019-10-23 16:16:04 -0400

    adds log regression with NN basics

--- 02-neural-networks-basics/logistic_regression_with_neural_network.py
+++ 02-neural-networks-basics/logistic_regression_with_neural_network.py
@@ -19,6 +19,7 @@ test_set_y shape: (1, 50)
 
 
 1 - Pre-process the data
+------------------------
 
 Common steps for pre-processing a new dataset are:
 
@@ -31,10 +32,69 @@ In this case we completely flatten everything. We previously had a train_set_x o
 *As always - the X dataset consists of a number of vertical instances - i.e. each Xi is a standard (vertical) vector of 12288 properties*
 
 
+
 2 - Consider the structure of the NN and do a forward pass
-Given that we have a single neuron in our netowrk, and each instance has 12288 properties, it means our weights (vertical) vector will consist of 12288 values in 1 column, and our bias value with be a scalar. 
+----------------------------------------------------------
+
+Given that we have a single neuron in our netowrk, and each instance has 12288 properties, it means our weights (vertical) vector will consist of 12288 values in 1 column, and our bias value with be a scalar. We initialize both with zeros.
+
+We now do a forward pass:
+
+# Calculate (w.T * X) + b, then pass it through s sigmoid function
+Activation = σ(np.dot(w.T, X) + b)
+		   = horizontal vector corresponding to the activations for each 	instance
+
+# Get total cost by comparing our activations with the correct Y values
+cost = -(1/m) * np.sum((Y*np.log(A) + (1-Y)*np.log(1-A)))
+	 = scalar (representing goodness of fit)
 
 
+# Calculate the gradients for each weight and also the bias, wrt to the cost for this batch of X. 
 
+dw = (1/m) * np.dot(X, (A-Y).T)
+   = [
+		[dw1],
+		[dw2],
+		[dw3],
+		[dw4],
+		 ...
+		[dwi],		
+    ]	
+   = vertical vector of all the weights for each of the 12288 properties
+
+We are multiplying the _rows_ of X with (A-Y).T, i.e. summing the dw1 gradients for all the instances, then summing the dw2 gradients for all instances, then summing the dw3 gradients etc. 
+Then we divide by 'm' to get the average gradient fo dw1, dw2, dw3, etc.
+
+We do the same for b:
+
+db = (1/m) * np.sum(A-Y)
+   = scalar
+
+
+3 - Optimize the weights and bias/es with gradient descent
+----------------------------------------------------------
+We then change the weights (and bias) in the opposite direction of the gradients in order to decrease the total cost:
+
+w = w - learning_rate * dw
+b = b - learning_rate * db
+
+Then run another forward pass to calculate costs, get the gradients, then optimize weights and bias again.
+
+
+4 - Use the optimized network to make predictions
+-------------------------------------------------
+Run an X vector/matrix through the network. If the activation is greater than 0.5, consider it a 1, otherwise consider it a 0.
 
 """
+
+
+
+
+
+
+
+
+
+
+
+
