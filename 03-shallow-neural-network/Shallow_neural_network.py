ca3dc03a5cbfc54b1bb5cfe04da6fc0e25436557
johnptmcdonald <johnptmcdonald@gmail.com>
2019-10-29 15:51:52 -0400

    notes for shallow NN

--- 03-shallow-neural-network/Shallow_neural_network.py
+++ 03-shallow-neural-network/Shallow_neural_network.py
@@ -7,46 +7,41 @@ A quick word on notation:
 Because we now have multiple neurons in multiple layers, the notation must be very specific. We use superscript to identify the neuron layer, and subscript to identify the neuron. 
 
 
-So our weights matrix will now be a matrix of column vectors, where each column represents the weights for a single neuron in the first layer:
+The notation used in this course is that W1 is equal to the already transposed weight matrix for the first layer.
 
-w = [
-	[0.4, 1.6],
-	[0.2, 1.7],
-	[0.1, 1.6],
-	[0.7, 1.9],
-]
+So if...
 
-i.e. the first neuron in the first layer has weights of...
-
-	[
-		[0.4],
-		[0.2],
-		[0.1],
-		[0.7]
+X =	[
+		[1, 2, 3],
+		[1, 2, 3]
 	]
 
-...and the second neuron in the first layer has weights of....
+i.e. each column represents an instance, each row is a feature
 
+Then the weights for the first layer are repesented as follows:
 
-	[
-		[1.6],
-		[1.7],
-		[1.6],
-		[1.9]
+W1 = [
+		[1, 1],		# weights for first neuron in first layer
+		[2, 2],		# weights for second neuron in first layer
+		[3, 3],		# weights for third neuron in first layer
+		[4, 4]		# weights for fourth neuron in first layer
 	]
 
+i.e. already transposed, so that... 
 
-b is now a column vector of biases:
-
-b = [
-	[1], // bias for first neuron
-	[2]	 // bias for second neuron
+W1 * X = [
+	[2, 4, 6],
+	[4, 8, 12],
+	[6, 12, 18],
+	[8, 16, 24] 
 ]
 
-Thus our raw activation is still np.dot(w.T, X) + b, which results in column vectors of raw activations for each neuron, which then has the b column vector added to it, which can then be sigmoided. 
+i.e. the first column is the activation for the first instance
 
 
-In general, different nodes in a layer are stacked vertically. 
+TASK - PLANAR DATA CLASSIFICATION
+---------------------------------
+
 
 """
 
