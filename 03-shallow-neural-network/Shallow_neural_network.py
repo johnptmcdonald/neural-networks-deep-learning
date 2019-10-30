44c7a63891e34a3128547180ebf3136463a6f7ea
johnptmcdonald <johnptmcdonald@gmail.com>
2019-10-30 11:55:53 -0400

    adds week 3 notes

--- 03-shallow-neural-network/Shallow_neural_network.py
+++ 03-shallow-neural-network/Shallow_neural_network.py
@@ -1,5 +1,9 @@
 
 """
+
+https://erjsarjplgszrwdfajaeto.coursera-apps.org/notebooks/Week%203/Planar%20data%20classification%20with%20one%20hidden%20layer/Planar_data_classification_with_onehidden_layer_v6c.ipynb
+
+
 A neural network is essentially the same calculations that were done in logistic regression, just repeated many times.
 
 A quick word on notation:
@@ -38,10 +42,31 @@ W1 * X = [
 
 i.e. the first column is the activation for the first instance
 
+A good general rule is that as we look at the neural network, each neuron is its own row in a matrix.
+
+
 
 TASK - PLANAR DATA CLASSIFICATION
 ---------------------------------
 
+We incrementally build a function called nn_model(X, Y, n_h, num_iterations, print_cost=False)
+
+This accepts the X and Y data, the number of hidden layers (n_h), and a couple of other params.
+
+STEP 1 - calculate n_x (input layer size, i.e. number of properties an instance of X has), and n_y (output layer size, i.e. number of output variables - for binary classification this is just 1) from X and Y
+
+STEP 2 - initialize parameters:
+Start with random weights for W1 and W2 and zero biases b1 and b2.
+
+STEP 3 - ITERATION
+Repeat the following steps many times.
+	- STEP 3a: forward_propagation(X, parameters of W1, W2, b1, and b2), which returns output activation A2 and a cache of all intermediate activations Z1, A1, Z2 (to be used later in backprop)
+	- STEP 3b: compute_cost(output A2, real Y) to get a measure of error for current parameters
+	- STEP 3c: perform backward_propagation(parameters, cache, X, and Y) to get gradients of all weight values at their current values
+	- STEP 3d: update parameters by moving in the opposite direction of their gradients
+
+STEP 4 - prediction(parameters, X):
+Do a forward propagation to get A2 (output activation), then binarize it to get 0 or 1. 
 
 """
 
